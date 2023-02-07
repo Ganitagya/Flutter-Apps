788238f7051e1e793f04380a0247e0df8b015faf
Akash Mahapatra <Ganitagya@users.noreply.github.com>
2023-02-07 20:03:21 +0400

    Camera Streaming - version 1 - able to get video on the screen

---
+++ MobileApps/camera_streaming/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return  Container();
+    }
+    return  Directionality(
+      textDirection: TextDirection.ltr,
+      child:  Stack(
+        children: <Widget>[
+           AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child:  CameraPreview(controller)),
+        ],
+      ),
+    );
+  }
+}
